cfce5223a660bbf8039de5b1797d873033a7148d
Utkarsh Tripathi <utripathi063@gmail.com>
2024-11-30 14:49:58 +0530

    Add files via upload

---
+++ uttu1.py
@@ -0,0 +1,10 @@
+# Simple Python program to check if the user is eligible to vote
+
+# Input: Ask the user for their age
+age = int(input("Please enter your age: "))
+
+# Conditional statement to check eligibility
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("Sorry, you are not eligible to vote yet.")
